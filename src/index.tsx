3c53dba87c916015e08e9e5f1d30a41964afd35e
Khagan <khaqan.rustamov@gmail.com>
2023-10-19 13:06:50 +0400

    used ts

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,13 @@ import { store } from "./redux/store";
 
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+}
