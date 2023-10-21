9a54ee46c7af80602564e09250d275a08e069b86
Khagan <khaqan.rustamov@gmail.com>
2023-10-21 11:23:32 +0400

    used TS

--- src/redux/slices/cartSlice.ts
+++ src/redux/slices/cartSlice.ts
@@ -1,12 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = {
+interface IItems {
+  id: number;
+  title?: string;
+  price?: number;
+  imageUrl?: string;
+  count?: number;
+  type: string;
+  size: number;
+}
+
+interface IInitialState {
+  items: IItems[];
+  totalPrice: number;
+  totalCount: number;
+}
+
+const initialState: IInitialState = {
   items: [],
   totalPrice: 0,
   totalCount: 0,
 };
 
-const findCartItem = (items, payload) => {
+const findCartItem = (items: IItems[], payload: IItems) => {
   return items.find(
     (item) =>
       item.id === payload.id &&
@@ -19,35 +35,38 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addItem(state, action) {
+    addItem(state, action: PayloadAction<IItems>) {
       const foundItem = findCartItem(state.items, action.payload);
 
-      if (foundItem) {
+      if (foundItem && foundItem.count) {
         foundItem.count++;
       } else {
         state.items.push({ ...action.payload, count: 1 });
       }
 
       state.totalPrice = state.items.reduce(
-        (acc, item) => acc + item.price * item.count,
+        (acc, item) =>
+          item.price && item.count ? acc + item.price * item.count : 0,
         0
       );
     },
 
-    minusItem(state, action) {
+    minusItem(state, action: PayloadAction<IItems>) {
       const foundItem = findCartItem(state.items, action.payload);
 
-      if (foundItem && foundItem.count > 0) {
+      if (foundItem && foundItem.count && foundItem.count > 0) {
         foundItem.count--;
-        state.totalPrice -= foundItem.price;
+        state.totalPrice -= foundItem.price ? foundItem.price : 0;
       }
     },
 
-    removeItem(state, action) {
+    removeItem(state, action: PayloadAction<IItems>) {
       const foundItem = findCartItem(state.items, action.payload);
 
-      if (foundItem) {
-        state.totalPrice -= foundItem.price * foundItem.count;
+      if (foundItem && foundItem.count) {
+        state.totalPrice -= foundItem.price
+          ? foundItem.price * foundItem.count
+          : 0;
         state.items = state.items.filter(
           (item) =>
             item.id !== action.payload.id ||
